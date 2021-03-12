15a8de6e8c79c759c4281b2d5e689faac4ba4998
runki <eeezgame@163.com>
2021-03-12 13:50:50 +0800

    修复展开/折叠bug 兼容了element2.7.0 -2.12.0版本

--- example/data.js
+++ example/data.js
@@ -51,3 +51,72 @@ const dataJson = [
     name: 'p'
   }
 ]
+
+const refreshDataJson = [
+  {
+    name: 'a',
+    children: [
+      {
+        name: 'b'
+      },
+      {
+        name: 'c',
+        children: [
+          {
+            name: 'd'
+          },
+          {
+            name: 'e',
+            children: [
+              { name: 'f' },
+              { name: 'g' },
+              { name: 'h' }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  
+  {
+    name: 'i'
+  },
+  {
+    name: 'j'
+
+  },
+  {
+    name: 'k'
+  },
+  {
+    name: 'm',
+
+    children: [
+      {
+        name: 'n'
+
+      },
+      {
+        name: 'o'
+
+      }
+    ]
+  },
+  {
+    name: 'M',
+
+    children: [
+      {
+        name: 'N'
+
+      },
+      {
+        name: 'O'
+
+      }
+    ]
+  },
+  {
+    name: 'p'
+  }
+]
--- src/vue-tree-table.vue
+++ src/vue-tree-table.vue
@@ -1,46 +1,111 @@
 <template>
-  <div class="app-container">
-    <el-table :data="tableData" :row-style="showTr" v-bind="$attrs" @selection-change="handleSelectionChange">
-      <el-table-column v-if="selection" type="selection" :width="selectionWidth">
-      </el-table-column>
-      <el-table-column align="center" label='ID' width="95">
-        <template slot-scope="scope">
-          <span :style="{paddingLeft:judegeLevel(scope.row)*levelPadding+'px'}">{{scope.row.id}}</span>
-          <el-button class="expand-button" type="text" size="mini" v-if="showExpanIcon(scope.row)" @click="toggleExpand(scope.$index)">
-            <i :class="scope.row._isExpanded?collapseIcon:expandIcon"></i>
-          </el-button>
-        </template>
-      </el-table-column>
-      <slot/>
-    </el-table>
-  </div>
+  <el-table
+    :data="data"
+    :row-style="showTr"
+    v-bind="$attrs"
+    @selection-change="handleSelectionChange"
+    @row-click="handleRowClick"
+    @header-click="handleHeaderClick"
+    size="mini"
+    ref="table"
+  >
+    <el-table-column v-if="selection" type="selection" :width="selectionWidth">
+    </el-table-column>
+    <slot name="left" />
+    <el-table-column
+      align="left"
+      :label="nodeFieldLabel || nodeField || 'ID'"
+      min-width="95"
+    >
+      <template slot-scope="scope">
+        <span
+          :style="{
+            paddingLeft: judegeLevel(scope.row) * levelPadding + 'px',
+            cursor: showExpanIcon(scope.row) ? 'pointer' : ''
+          }"
+          @click.stop="toggleExpand(scope.$index)"
+          >{{ scope.row[nodeField] || scope.row.id }}</span
+        >
+        <el-button
+          class="expand-button"
+          type="text"
+          size="mini"
+          v-if="showExpanIcon(scope.row)"
+          @click.stop="toggleExpand(scope.$index)"
+        >
+          <i
+            :class="scope.row._isExpanded ? collapseIcon : expandIcon"
+            class="f-fwb"
+          ></i>
+        </el-button>
+      </template>
+    </el-table-column>
+    <slot />
+  </el-table>
 </template>
-
-
-
 <script>
-
+// 兼容性处理
+// import { version } from "element-ui";
+const semverGte = require('semver/functions/gte')
+const semverLte = require('semver/functions/lte')
+let version = ''
+if (typeof window !== 'undefined' && window.ELEMENT) {
+  version  = window.ELEMENT.version
+}
+/* process.env.NODE_ENV !== "production" &&
+  semverGte(version,"2.7.0") &&
+  semverLte(version , "2.12.0")
+  console.warn(
+    "element-ui版本在2.7.0-2.12.0中,tablebody.vue 指令 v-show 会覆盖 row-style 中 display,因此用z-index进行隐藏.若要更好的渲染效率,请把element-ui升级到2.13.0及以上."
+  ); */
+const trHidden =
+  semverGte(version,"2.7.0") && semverLte(version , "2.12.0")
+    ? {
+        position: "absolute",
+        "z-index": -999
+      }
+    : { display: "none" };
 export default {
   props: {
-    data: {
+    /* data: {
       type: Array,
-      default: []
+      default: () => []
+    }, */
+    nodeField: {
+      type: String,
+      default: ""
+    },
+    nodeFieldLabel: {
+      type: String,
+      default: ""
+    },
+    defaultExpandAll: {
+      type: Boolean,
+      default: false
+    },
+    defaultChildren: {
+      type: String,
+      default: "children"
     },
     levelPadding: {
       type: Number,
       default: 20
     },
     expandAnimation: {
-      type: String,
-      default: 'animation:treeTableShow 1s;'
+      type: Object,
+      default: () => {
+        return {
+          animation: "treeTableShow 1s"
+        };
+      }
     },
     expandIcon: {
       type: String,
-      default: 'el-icon-plus'
+      default: "el-icon-plus"
     },
     collapseIcon: {
       type: String,
-      default: 'el-icon-minus'
+      default: "el-icon-minus"
     },
     selection: {
       type: Boolean,
@@ -54,80 +119,126 @@ export default {
   data() {
     return {
       listLoading: false,
-      tableData: []
-    }
+      data: []
+    };
   },
-
   created() {
-    this.tableData = this.formatData(this.data)
   },
-
   methods: {
+    render(dataSource){
+      this.data = this.formatData(dataSource);
+    },
     formatData(data, parentId) {
-      let tempData = []
+      let tempData = [];
+      const defaultExpandAll = this.defaultExpandAll;
       data.forEach((item, index) => {
         if (parentId !== undefined) {
-          this.$set(item, '_isExpanded', false)
-          this.$set(item, '_isShow', false)
-          this.$set(item, '_parentId', parentId + '')
-          this.$set(item, '_id', parentId + '-' + index)
+          this.$set(item, "_isExpanded", defaultExpandAll);
+          this.$set(item, "_isShow", defaultExpandAll);
+          this.$set(item, "_parentId", parentId + "");
+          this.$set(item, "_id", parentId + "-" + index);
         } else {
-          this.$set(item, '_isExpanded', false)
-          this.$set(item, '_isShow', true)
-          this.$set(item, '_id', index + '')
+          this.$set(item, "_isExpanded", defaultExpandAll);
+          this.$set(item, "_isShow", true);
+          this.$set(item, "_id", index + "");
         }
-        if (item.children && item.children.length > 0) {
-          tempData.push(item)
-          tempData = tempData.concat(this.formatData(item.children, parentId ? parentId + '-' + index : index + ''))
+        if (
+          item[this.defaultChildren] &&
+          item[this.defaultChildren].length > 0
+        ) {
+          tempData.push(item);
+          tempData = tempData.concat(
+            this.formatData(
+              item[this.defaultChildren],
+              parentId ? parentId + "-" + index : index + ""
+            )
+          );
         } else {
-          tempData.push(item)
+          tempData.push(item);
         }
-      })
-      console.log(tempData)
-      return tempData
+      });
+      return tempData;
     },
     showTr: function({ row, index }) {
-      const show = (row._parentId === undefined) || row._isShow
-      return show ? this.expandAnimation : 'display:none;'
+      const show = row._parentId === undefined || row._isShow;
+      return show ? this.expandAnimation : trHidden;
     },
     toggleExpand: function(trIndex) {
-      const record = this.tableData[trIndex]
-      record._isExpanded = !record._isExpanded
-
-      const arr = this.tableData.filter(i => {
-        return i._parentId === record._id
-      })
-      console.log(arr)
+      const record = this.data[trIndex];
+      record._isExpanded = !record._isExpanded;
+      const arr = this.data.filter(i => {
+        return i._parentId === record._id;
+      });
       arr.forEach(i => {
-        i._isShow = !i._isShow
-      })
+        i._isShow = !i._isShow;
+      });
+      // beta代码
+      record._isExpanded
+        ? this.showExpandedChildren(arr)
+        : this.hiddenChildren(arr);
+      this.$nextTick(() => {
+        this.$refs.table.doLayout();
+      });
+    },
+    // beta
+    hiddenChildren: function(childrens) {
+      childrens.map(children => {
+        children._isShow = false;
+        // children._isExpanded = false;
+        children.operator ? "" : (children._isExpanded = false);
+        children.operator ? this.hiddenChildren(children.operator) : "";
+      });
+    },
+    // beta
+    showExpandedChildren: function(childrens, parent) {
+      childrens.map(children => {
+        if (parent && parent.operator && parent._isExpanded && parent._isShow) {
+          children._isShow = true;
+          childrens._isExpanded = true;
+        }
+        children.operator
+          ? this.showExpandedChildren(children.operator, children)
+          : "";
+      });
     },
     showExpanIcon(record) {
-      if (record.children && record.children.length > 0) {
-        return true
+      if (
+        record[this.defaultChildren] &&
+        record[this.defaultChildren].length > 0
+      ) {
+        return true;
       }
-      return false
+      return false;
     },
     judegeLevel(row) {
-      return (row._id + '').split('-').length - 1
+      return (row._id + "").split("-").length - 1;
     },
     handleSelectionChange(val) {
-      this.$emit('selection-change', val)
+      this.$emit("selection-change", val);
+    },
+    handleRowClick(val, column, event) {
+      this.$emit("row-click", val);
+    },
+    handleHeaderClick(val) {
+      this.$emit("header-click", val);
     }
   }
-}
+};
 </script>
 
 <style>
 @keyframes treeTableShow {
-    from {opacity: 0;}
-    to {opacity: 1;}
+  from {
+    opacity: 0;
   }
+  to {
+    opacity: 1;
+  }
+}
 </style>
 
 <style scoped>
-
-.expand-button{
+/* .expand-button {
   margin-left: -20px;
-}
+} */
 </style>
